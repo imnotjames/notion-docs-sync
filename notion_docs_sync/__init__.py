77e1c118246337e452a0d1e49f0f33a518c3a376
James Ward <james@notjam.es>
2020-03-18 20:06:30 -0400

    add poetry manifest + code

---
+++ notion_docs_sync/__init__.py
@@ -0,0 +1,159 @@
+import sys
+import os
+from random import choice
+from argparse import ArgumentParser
+
+from notion.client import NotionClient
+from notion.block import Block, PageBlock, CollectionViewBlock
+from emoji import EMOJI_UNICODE
+import frontmatter
+
+from .markdown import convert
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
+
+
+def random_emoji():
+    # Don't allow people, hands, or fingers.
+    forbidden_emoji_patterns = ['child', 'skin_tone', 'person', 'hand', 'finger']
+
+    emoji_key = None
+    while not emoji_key:
+        emoji_key = choice(list(EMOJI_UNICODE.keys()))
+
+        for pattern in forbidden_emoji_patterns:
+            if pattern in emoji_key:
+                emoji_key = None
+                break
+
+    return EMOJI_UNICODE[emoji_key]
+
+
+def infer_block(root_block, path) -> Block:
+
+    name, ext = os.path.splitext(path)
+
+    if name == 'index':
+        return root_block
+
+    if ext != '.md' and ext != '':
+        return None
+
+    title = name.replace('-', ' ').replace('_', ' ').capitalize()
+
+    for block in root_block.children:
+        if block.type != 'page':
+            continue
+
+        if block.title != title:
+            continue
+
+        return block
+
+    # Create a new page block
+
+    return root_block.children.add_new(PageBlock, title=title)
+
+
+def sync_file_to_block(filename, block):
+    with open(filename) as markdown_fd:
+        contents = markdown_fd.read()
+
+    post = frontmatter.loads(contents)
+
+    markdown_blocks = convert(str(post))
+
+    if block.icon is None:
+        block.icon = random_emoji()
+
+    # TODO: Don't remove blocks that match?
+    # Remove non-page blocks.
+    for c in block.children:
+        if c.type != 'page':
+            c.remove()
+
+    for markdown_block in reversed(markdown_blocks):
+        markdown_block_class = markdown_block["type"]
+        del markdown_block["type"]
+
+        if "schema" in markdown_block:
+            collection_schema = markdown_block["schema"]
+            collection_rows = markdown_block["rows"]
+            del markdown_block["schema"]
+            del markdown_block["rows"]
+
+        block_children = None
+        if "children" in markdown_block:
+            block_children = markdown_block["children"]
+            del markdown_block["children"]
+
+        new_block = block.children.add_new(markdown_block_class, **markdown_block)
+
+        new_block.move_to(block, 'first-child')
+
+        if isinstance(new_block, CollectionViewBlock):
+                #We should have generated a schema and rows for this one
+                client = block._client #Hacky internals stuff...
+                new_block.collection = client.get_collection(
+                    #Low-level use of the API
+                    #TODO: Update when notion-py provides a better interface for this
+                    client.create_record("collection", parent=new_block, schema=collection_schema)
+                )
+
+                new_block.views.add_new(view_type="table")
+
+                for row in collection_rows:
+                    newRow = new_block.collection.add_row()
+                    for idx, propName in enumerate(prop["name"] for prop in collection_schema.values()):
+                        # TODO: If rows aren't uploading, check to see if there's special
+                        # characters that don't map to propName in notion-py
+                        propName = propName.lower() #The actual prop name in notion-py is lowercase
+                        propVal = row[idx]
+                        setattr(newRow, propName, propVal)
+
+
+def sync_directory_to_block(directory, root_block):
+    touched_pages = set()
+
+    for path in os.listdir(directory):
+        block = infer_block(root_block, path)
+
+        if not block:
+            continue
+
+        touched_pages.add(block.id)
+
+        full_path = os.path.join(directory, path)
+
+        if os.path.isdir(full_path):
+            sync_directory_to_block(full_path, block)
+        else:
+            sync_file_to_block(full_path, block)
+
+    # Any children that are pages under root_block but aren't in touched_pages should be pruned
+    for child in root_block.children:
+        if child.type == 'page' and child.id not in touched_pages:
+            child.remove()
+
+def main():
+    parser = ArgumentParser()
+
+    parser.add_argument('--notion-token', type=str, default=os.environ.get('NOTION_TOKEN'))
+    parser.add_argument('docs_path', type=str)
+    parser.add_argument('notion_url', type=str)
+
+    args = parser.parse_args()
+
+    token = args.notion_token
+    root_url = args.notion_url
+    docs_path = args.docs_path
+
+    # add row to notion collection and add a text block with link to the new card
+    client = NotionClient(token_v2=token)
+    root_block = client.get_block(root_url)
+
+    sync_directory_to_block(docs_path, root_block)
---
+++ notion_docs_sync/markdown.py
@@ -0,0 +1,308 @@
+import random
+import re
+import collections
+from notion.block import CodeBlock, DividerBlock, HeaderBlock, SubheaderBlock, \
+    SubsubheaderBlock, QuoteBlock, TextBlock, NumberedListBlock, \
+    BulletedListBlock, ImageBlock, CollectionViewBlock
+import mistletoe
+from mistletoe.base_renderer import BaseRenderer
+from mistletoe.span_token import Image, Link
+
+def flatten(l):
+    for el in l:
+        if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes, dict)):
+            yield from flatten(el)
+        else:
+            yield el
+
+class NotionRenderer(BaseRenderer):
+    """
+    A class that will render out a Markdown file into a descriptor for upload
+    with notion-py. Each object will have a .type for the block type and then
+    a bunch of different dict entries corresponding to kwargs for that block
+    type.
+    For CollectionViewBlocks, a .rows entry exists in the dictionary with a list
+    object containing a descriptor for every row. This is still TODO
+    """
+
+    def render(self, token):
+        """
+        Takes a single Markdown token and renders it down to
+        NotionPy classes. Note that all the recursion is handled in the delegated
+        methods.
+        Overrides super().render but still uses render_map and then just
+        does special parsing for stuff
+        """
+        return self.render_map[token.__class__.__name__](token)
+
+    def renderMultiple(self, tokens):
+        """
+        Takes an array of sibling tokens and renders each one out.
+        """
+        return list(flatten(self.render(t) for t in tokens))
+
+    def renderMultipleToString(self, tokens):
+        """
+        Takes tokens and render them to a single string (if possible). Anything it
+        can't convert to a string will be returned in the second part of the tuple
+        @param {objects} tokens
+        @returns {tuple} (str, dict[])
+        """
+        def toString(renderedBlock):
+            if isinstance(renderedBlock, dict) and renderedBlock['type'] == TextBlock:
+                return renderedBlock['title'] #This unwraps TextBlocks/paragraphs to use in other blocks
+            else: #Returns str as-is or returns blocks we can't convert
+                return renderedBlock
+
+        #Try to convert any objects to strings
+        rendered = [ toString(b) for b in self.renderMultiple(tokens)]
+        strs = "".join([s for s in rendered if isinstance(s, str)])
+        blocks = [b for b in rendered if isinstance(b, dict)]
+        #Return a tuple of strings and any extra blocks we couldn't convert
+        return (strs, blocks)
+
+    def renderMultipleToStringAndCombine(self, tokens, toBlockFunc):
+        """
+        renderMultipleToString but combines the string with the other blocks
+        with the returned block from toBlockFunc
+        @param {objects} tokens
+        @param {function} toBlockFunc Takes a str and returns a dict for the created
+        @returns {dict[]}
+        """
+        strs, blocks = self.renderMultipleToString(tokens)
+        ret = []
+        if strs: #If a non-empty string block
+            ret = ret + [toBlockFunc(strs)]
+        if blocks:
+            ret = ret + blocks
+        return ret
+
+    def render_document(self, token):
+        return self.renderMultiple(token.children)
+
+    # == MD Block Tokens ==
+    def render_block_code(self, token):
+        #Indented code and ``` ``` code fence
+        #Notion seems really picky about the language field and the case sensitivity
+        #so we match the string to the specific version that Notion.so expects
+        notionSoLangs = [
+            "ABAP",
+            "Arduino",
+            "Bash",
+            "BASIC",
+            "C",
+            "Clojure",
+            "CoffeeScript",
+            "C++",
+            "C#",
+            "CSS",
+            "Dart",
+            "Diff",
+            "Docker",
+            "Elixir",
+            "Elm",
+            "Erlang",
+            "Flow",
+            "Fortran",
+            "F#",
+            "Gherkin",
+            "GLSL",
+            "Go",
+            "GraphQL",
+            "Groovy",
+            "Haskell",
+            "HTML",
+            "Java",
+            "JavaScript",
+            "JSON",
+            "Kotlin",
+            "LaTeX",
+            "Less",
+            "Lisp",
+            "LiveScript",
+            "Lua",
+            "Makefile",
+            "Markdown",
+            "Markup",
+            "MATLAB",
+            "Nix",
+            "Objective-C",
+            "OCaml",
+            "Pascal",
+            "Perl",
+            "PHP",
+            "Plain Text",
+            "PowerShell",
+            "Prolog",
+            "Python",
+            "R",
+            "Reason",
+            "Ruby",
+            "Rust",
+            "Sass",
+            "Scala",
+            "Scheme",
+            "Scss",
+            "Shell",
+            "SQL",
+            "Swift",
+            "TypeScript",
+            "VB.Net",
+            "Verilog",
+            "VHDL",
+            "Visual Basic",
+            "WebAssembly",
+            "XML",
+            "YAML"
+        ]
+        if token.language != "":
+            matchLang = next((lang for lang in notionSoLangs if re.match(re.escape(token.language), lang, re.I)), "")
+            if not matchLang:
+                print(f"Code block language {token.language} has no corresponding syntax in Notion.so")
+        else:
+            matchLang = "Plain Text"
+
+        def blockFunc(blockStr):
+            return {
+                'type': CodeBlock,
+                'language': matchLang,
+                'title_plaintext': blockStr
+            }
+        return self.renderMultipleToStringAndCombine(token.children, blockFunc)
+
+    def render_thematic_break(self, token):
+        return {
+            'type': DividerBlock
+        }
+    def render_heading(self, token):
+        level = token.level
+        if level > 3:
+            print(f"h{level} not supported in Notion.so, converting to h3")
+            level = 3
+
+        def blockFunc(blockStr):
+            return {
+                'type': [HeaderBlock, SubheaderBlock, SubsubheaderBlock][level-1],
+                'title': blockStr
+            }
+        return self.renderMultipleToStringAndCombine(token.children, blockFunc)
+    def render_quote(self, token):
+        def blockFunc(blockStr):
+            return {
+                'type': QuoteBlock,
+                'title': blockStr
+            }
+        return self.renderMultipleToStringAndCombine(token.children, blockFunc)
+    def render_paragraph(self, token):
+        def blockFunc(blockStr):
+            return {
+                'type': TextBlock,
+                'title': blockStr
+            }
+        return self.renderMultipleToStringAndCombine(token.children, blockFunc)
+    def render_list(self, token):
+        #List items themselves are each blocks, so skip it and directly render
+        #the children
+        return self.renderMultiple(token.children)
+    def render_list_item(self, token):
+        leaderContainsNumber = re.match(r'\d', token.leader) #Contains a number
+
+        #Lists can have "children" (nested lists, nested images...), so we need
+        #to render out all the nodes and sort through them to find the string
+        #for this item and any children
+        rendered = self.renderMultiple(token.children)
+        children = [b for b in rendered if b['type'] != TextBlock]
+        strings = [s['title'] for s in rendered if s['type'] == TextBlock]
+
+        return {
+            'type': NumberedListBlock if leaderContainsNumber else BulletedListBlock,
+            'title': "".join(strings),
+            'children': children
+        }
+    def render_table(self, token):
+        headerRow = self.render(token.header) #Header is a single row
+        rows = [self.render(r) for r in token.children] #don't use renderMultiple because it flattens
+
+        def randColId():
+            def randChr():
+                #ASCII 32 - 126 is ' ' to '~', all printable characters
+                return chr(random.randrange(32,126))
+            #4 characters long of random printable characters, I don't think it
+            #has any correlation to the data?
+            return "".join([randChr() for c in range(4)])
+        def textColSchema(colName):
+            return { 'name' : colName, 'type': 'text' }
+        #The schema is basically special identifiers + the type of property
+        #to put into Notion. Coming from Markdown, everything is going to
+        #be text.
+        # 'J=}x': {
+        #     'name': 'Column',
+        #     'type': 'text'
+        # },
+        schema = { randColId() : textColSchema(headerRow[r]) for r in range(len(headerRow) - 1) }
+        #The last one needs to be named 'Title' and is type title
+        # 'title': {
+        #     'name': 'Name',
+        #     'type': 'title'
+        # }
+        schema.update({
+                'title' : {
+                    'name': headerRow[-1],
+                    'type': 'title'
+                }
+            })
+
+        #CollectionViewBlock, and it's gonna be a bit hard to do because this
+        #isn't fully fleshed out in notion-py yet but we can still use create_record
+        return {
+            'type': CollectionViewBlock,
+            'rows': rows, #everything except the initial row
+            'schema': schema
+        }
+    def render_table_row(self, token):
+        #Rows are a concept in Notion (`CollectionRowBlock`) but notion-py provides
+        #another API to use it, `.add_row()` so we just render down to an array
+        #and handle in the Table block.
+        return self.renderMultiple(token.children)
+    def render_table_cell(self, token):
+        #Render straight down to a string, cells aren't a concept in Notion
+        strs, blocks = self.renderMultipleToString(token.children)
+        if blocks:
+            print("Table cell contained non-strings (maybe an image?) and could not add...")
+        return strs
+
+    # == MD Span Tokens ==
+    # These tokens always appear inside another block-level token (so we can return
+    # a string instead of a block if necessary). Most of these are handled by
+    # notion-py's uploader as it will convert them to the internal Notion.so
+    # MD-like formatting
+    def render_strong(self, token):
+        return self.renderMultipleToStringAndCombine(token.children, lambda s: f"**{s}**")
+    def render_emphasis(self, token):
+        return self.renderMultipleToStringAndCombine(token.children, lambda s: f"*{s}*")
+    def render_inline_code(self, token):
+        return self.renderMultipleToStringAndCombine(token.children, lambda s: f"`{s}`")
+    def render_raw_text(self, token):
+        return token.content
+    def render_strikethrough(self, token):
+        return self.renderMultipleToStringAndCombine(token.children, lambda s: f"~{s}~")
+    def render_link(self, token):
+        strs, blocks = self.renderMultipleToString(token.children)
+        return [ f"[{strs}]({token.target})" ] + blocks
+    def render_escape_sequence(self, token):
+        #Pretty sure this is just like \xxx type escape sequences?
+        return self.renderMultipleToStringAndCombine(token.children, lambda s: f"\\{s}")
+    def render_line_break(self, token):
+        return '\n'
+    def render_image(self, token):
+        #Alt text
+        alt = token.title or self.renderMultipleToString(token.children)[0]
+        return {
+            'type': ImageBlock,
+            'display_source': token.src,
+            'source': token.src,
+            'caption': alt
+        }
+
+def convert(markdown):
+    return mistletoe.markdown(markdown, renderer=NotionRenderer)
